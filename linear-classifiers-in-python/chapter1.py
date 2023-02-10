3529ccc237d07492ae140851ec93d317a071acc2
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-10 09:01:14 +0200

    Update chapter1.py

--- linear-classifiers-in-python/chapter1.py
+++ linear-classifiers-in-python/chapter1.py
@@ -7,3 +7,118 @@ knn.fit(X_train, y_train)
 # Predict on the test features, print the results
 pred = knn.predict(X_test)[0]
 print("Prediction for test example 0:", pred)
+------------
+# overfitting referes to doing better on the training set than the test set.
+# Training accuracy 95%, testing accuracy 50%.
+
+-------------
+from sklearn import datasets
+digits = datasets.load_digits()
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target)
+
+# Apply logistic regression and print scores
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+print(lr.score(X_train, y_train))
+print(lr.score(X_test, y_test))
+
+# Apply SVM and print scores
+svm = SVC()
+svm.fit(X_train, y_train)
+print(svm.score(X_train, y_train))
+print(svm.score(X_test, y_test))
+
+-----------------
+# Instantiate logistic regression and train
+lr = LogisticRegression()
+lr.fit(X,y)
+
+# Predict sentiment for a glowing review
+review1 = "LOVED IT! This movie was amazing. Top 10 this year."
+review1_features = get_features(review1)
+print("Review:", review1)
+print("Probability of positive review:", lr.predict_proba(review1_features)[0,1])
+
+# Predict sentiment for a poor review
+review2 = "Total junk! I'll never watch a film by that director again, no matter how good the reviews."
+review2_features = get_features(review2)
+print("Review:", review2)
+print("Probability of positive review:", lr.predict_proba(review2_features)[0,1])
+
+-------------
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+
+# Define the classifiers
+classifiers = [LogisticRegression(), LinearSVC(), SVC(), KNeighborsClassifier()]
+
+# Fit the classifiers
+for c in classifiers:
+    c.fit(X,y)
+
+# Plot the classifiers
+plot_4_classifiers(X, y, classifiers)
+plt.show()
+
+---------------
+# Set the coefficients
+model.coef_ = np.array([[-2,1]])
+model.intercept_ = np.array([-4])
+
+# Plot the data and decision boundary
+plot_classifier(X,y,model)
+
+# Print the number of errors
+num_err = np.sum(y != model.predict(X))
+print("Number of errors:", num_err)
+-----------
+
+# The squared error, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(y.size):
+        # Get the true and predicted target values for example 'i'
+        y_i_true = y[i]
+        y_i_pred = w@X[i]
+        s = s + (y_i_true - y_i_pred)**2
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LinearRegression coefficients
+lr = LinearRegression(fit_intercept=False).fit(X,y)
+print(lr.coef_)
+
+------------------
+# Mathematical functions for logistic and hinge losses
+def log_loss(raw_model_output):
+   return np.log(1+np.exp(-raw_model_output))
+def hinge_loss(raw_model_output):
+   return np.maximum(0,1-raw_model_output)
+
+# Create a grid of values and plot
+grid = np.linspace(-2,2,1000)
+plt.plot(grid, log_loss(grid), label='logistic')
+plt.plot(grid, hinge_loss(grid), label='hinge')
+plt.legend()
+plt.show()
+
+--------------
+# The logistic loss, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(y.shape[0]):
+        raw_model_output = w@X[i]
+        s = s + log_loss(raw_model_output * y[i])
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LogisticRegression
+lr = LogisticRegression(fit_intercept=False, C=1000000).fit(X,y)
+print(lr.coef_)
