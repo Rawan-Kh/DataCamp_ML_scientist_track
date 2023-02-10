1e4a5b3fb805750c73136d7677c17b063d249da5
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-10 20:47:09 +0200

    Create chapter4.py

---
+++ linear-classifiers-in-python/chapter4.py
@@ -0,0 +1,64 @@
+# Train a linear SVM
+svm = SVC(kernel="linear")
+svm.fit(X, y)
+plot_classifier(X, y, svm, lims=(11,15,0,6))
+
+# Make a new data set keeping only the support vectors
+print("Number of original examples", len(X))
+print("Number of support vectors", len(svm.support_))
+X_small = X[svm.support_]
+y_small = y[svm.support_]
+
+# Train a new SVM using only the support vectors
+svm_small = SVC(kernel="linear")
+svm_small.fit(X, y)
+plot_classifier(X_small, y_small, svm_small, lims=(11,15,0,6))
+
+------------
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X , y)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+#  Larger values of gamma are better for training accuracy, but cross-validation helped us find something different (and better!)
+------------
+
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'C':[0.1, 1, 10], 'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+
+# Report the test accuracy using these best parameters
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
+
+-----------
+# We set random_state=0 for reproducibility 
+linear_classifier = SGDClassifier(random_state=0)
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'alpha':[0.00001, 0.0001, 0.001, 0.01, 0.1, 1], 
+             'loss':['hinge','log_loss']}
+searcher = GridSearchCV(linear_classifier, parameters, cv=10)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
+
+# you finished the last exercise in the course! One advantage of SGDClassifier is that it's very fast - 
+# this would have taken a lot longer with LogisticRegression or LinearSVC
+
