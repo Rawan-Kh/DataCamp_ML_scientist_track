015d39dcdd089b076fb88eb8e6d46ccd76644fa1
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-10 20:26:20 +0200

    Create chapter3.py

---
+++ linear-classifiers-in-python/chapter3.py
@@ -0,0 +1,145 @@
+# Train and validaton errors initialized as empty list
+train_errs = list()
+valid_errs = list()
+
+# Loop over values of C_value
+for C_value in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
+    # Create LogisticRegression object and fit
+    lr = LogisticRegression(C=C_value)
+    lr.fit(X_train, y_train)
+    
+    # Evaluate error rates and append to lists
+    train_errs.append( 1.0 - lr.score(X_train, y_train) )
+    valid_errs.append( 1.0 - lr.score(X_valid, y_valid) )
+    
+# Plot results
+plt.semilogx(C_values, train_errs, C_values, valid_errs)
+plt.legend(("train", "validation"))
+plt.show()
+
+-------
+# Specify L1 regularization
+lr = LogisticRegression(solver='liblinear',penalty='l1')
+
+# Instantiate the GridSearchCV object and run the search
+searcher = GridSearchCV(lr, {'C':[0.001, 0.01, 0.1, 1, 10]})
+searcher.fit(X_train, y_train)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+# Find the number of nonzero coefficients (selected features)
+best_lr = searcher.best_estimator_
+coefs = best_lr.coef_
+print("Total number of features:", coefs.size)
+print("Number of selected features:", np.count_nonzero(coefs))
+
+# As you can see, a whole lot of features were discarded here.
+-------------
+
+# Get the indices of the sorted cofficients
+inds_ascending = np.argsort(lr.coef_.flatten()) 
+inds_descending = inds_ascending[::-1]
+
+# Print the most positive words
+print("Most positive words: ", end="")
+for i in range(5):
+    print(vocab[inds_descending][i], end=", ")
+print("\n")
+
+# Print most negative words
+print("Most negative words: ", end="")
+for i in range(5):
+    print(vocab[inds_ascending][i], end=", ")
+print("\n")
+---------------
+# Set the regularization strength
+model = LogisticRegression(C=1)
+
+# Fit and plot
+model.fit(X,y)
+plot_classifier(X,y,model,proba=True)
+
+# Predict probabilities on training points
+prob = model.predict_proba(X)
+print("Maximum predicted probability", np.max(prob))
+-------
+# Set the regularization strength
+model = LogisticRegression(C=0.1)
+
+# Fit and plot
+model.fit(X,y)
+plot_classifier(X,y,model,proba=True)
+
+# Predict probabilities on training points
+prob = model.predict_proba(X)
+print("Maximum predicted probability", np.max(prob))
+
+# As you probably noticed, smaller values of C lead to less confident predictions. That's because smaller C means more regularization,
+# which in turn means smaller coefficients, which means raw model outputs closer to zero and, thus, 
+# probabilities closer to 0.5 after the raw model output is squashed through the sigmoid function. That's quite a chain of events!
+---------
+# Train a linear SVM
+svm = SVC(kernel="linear")
+svm.fit(X, y)
+plot_classifier(X, y, svm, lims=(11,15,0,6))
+
+# Make a new data set keeping only the support vectors
+print("Number of original examples", len(X))
+print("Number of support vectors", len(svm.support_))
+X_small = X[svm.support_]
+y_small = y[svm.support_]
+
+# Train a new SVM using only the support vectors
+svm_small = SVC(kernel="linear")
+svm_small.fit(X, y)
+plot_classifier(X_small, y_small, svm_small, lims=(11,15,0,6))
+
+------------
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X , y)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+#  Larger values of gamma are better for training accuracy, but cross-validation helped us find something different (and better!)
+------------
+
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'C':[0.1, 1, 10], 'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+
+# Report the test accuracy using these best parameters
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
+
+-----------
+# We set random_state=0 for reproducibility 
+linear_classifier = SGDClassifier(random_state=0)
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'alpha':[0.00001, 0.0001, 0.001, 0.01, 0.1, 1], 
+             'loss':['hinge','log_loss']}
+searcher = GridSearchCV(linear_classifier, parameters, cv=10)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
+
+# you finished the last exercise in the course! One advantage of SGDClassifier is that it's very fast - 
+# this would have taken a lot longer with LogisticRegression or LinearSVC
+
