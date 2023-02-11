f0f9c2eb7dddde976746e84dcade0f411625be6b
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-11 06:54:26 +0200

    Create chapter1.py

---
+++ machine-learning-with-tree-based-models-in-python/chapter1.py
@@ -0,0 +1,118 @@
+# Import DecisionTreeClassifier from sklearn.tree
+from sklearn.tree import DecisionTreeClassifier
+
+# Instantiate a DecisionTreeClassifier 'dt' with a maximum depth of 6
+dt = DecisionTreeClassifier(max_depth=6, random_state=SEED)
+
+# Fit dt to the training set
+dt.fit(X_train,y_train)
+
+# Predict test set labels
+y_pred = dt.predict(X_test)
+print(y_pred[0:5])
+
+--------
+# Import accuracy_score
+from  sklearn.metrics import accuracy_score
+
+# Predict test set labels
+y_pred = dt.predict(X_test)
+
+# Compute test set accuracy  
+acc = accuracy_score(y_test, y_pred)
+print("Test set accuracy: {:.2f}".format(acc))
+
+# Using only two features, your tree was able to achieve an accuracy of 89%
+
+--------------------
+# Import LogisticRegression from sklearn.linear_model
+from sklearn.linear_model import  LogisticRegression
+
+# Instatiate logreg
+logreg = LogisticRegression(random_state=1)
+
+# Fit logreg to the training set
+logreg.fit(X_train, y_train)
+
+# Define a list called clfs containing the two classifiers logreg and dt
+clfs = [logreg, dt]
+
+# Review the decision regions of the two classifiers
+plot_labeled_decision_regions(X_test, y_test, clfs)
+
+# Notice how the decision boundary produced by logistic regression is linear 
+# while the boundaries produced by the classification tree divide the feature space into rectangular regions
+
+----------------
+# Import DecisionTreeClassifier from sklearn.tree
+from sklearn.tree import DecisionTreeClassifier
+
+# Instantiate dt_entropy, set 'entropy' as the information criterion
+dt_entropy = DecisionTreeClassifier(max_depth=8, criterion='entropy', random_state=1)
+
+# Fit dt_entropy to the training set
+dt_entropy.fit(X_train , y_train)
+
+-------------
+# Import accuracy_score from sklearn.metrics
+from sklearn.metrics import accuracy_score
+
+# Use dt_entropy to predict test set labels
+y_pred= dt_entropy.predict(X_test)
+
+# Evaluate accuracy_entropy
+accuracy_entropy = accuracy_score(y_test, y_pred)
+
+# Print accuracy_entropy
+print(f'Accuracy achieved by using entropy: {accuracy_entropy:.3f}')
+
+# Print accuracy_gini
+print(f'Accuracy achieved by using the gini index: {accuracy_gini:.3f}')
+
+# Notice how the two models achieve almost the same accuracy. Most of the time, the gini index and entropy lead to the same results. 
+# The gini index is slightly faster to compute and is the default criterion used in the DecisionTreeClassifier model of scikit-learn
+
+------------------
+# Import DecisionTreeRegressor from sklearn.tree
+from sklearn.tree import DecisionTreeRegressor
+
+# Instantiate dt
+dt = DecisionTreeRegressor(max_depth=8,
+             min_samples_leaf=0.13,
+            random_state=3)
+
+# Fit dt to the training set
+dt.fit(X_train , y_train)
+
+------------------
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute y_pred
+y_pred = dt.predict(X_test)
+
+# Compute mse_dt
+mse_dt = MSE(y_test,y_pred)
+
+# Compute rmse_dt
+rmse_dt = mse_dt**(1/2)
+
+# Print rmse_dt
+print("Test set RMSE of dt: {:.2f}".format(rmse_dt))
+
+------------------
+# Predict test set labels 
+y_pred_lr = lr.predict(X_test)
+
+# Compute mse_lr
+mse_lr = MSE(y_test, y_pred_lr)
+
+# Compute rmse_lr
+rmse_lr = mse_lr**(1/2)
+
+# Print rmse_lr
+print('Linear Regression test set RMSE: {:.2f}'.format(rmse_lr))
+
+# Print rmse_dt
+print('Regression Tree test set RMSE: {:.2f}'.format(rmse_dt))
+
