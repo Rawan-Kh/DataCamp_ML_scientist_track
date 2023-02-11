12b6aa92282c409efc4c8b5420d4eb1292d7b2bd
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-11 19:37:14 +0200

    Create chapter4.py

---
+++ feature-engineering-for-machine-learning-in-python/chapter4.py
@@ -0,0 +1,138 @@
+# Print the first 5 rows of the text column
+print(speech_df['text'].head())
+-------
+# Replace all non letter characters with a whitespace
+speech_df['text_clean'] = speech_df['text'].str.replace('[^a-zA-Z]', ' ')
+
+# Change to lower case
+speech_df['text_clean'] = speech_df['text_clean'].str.lower()
+
+# Print the first 5 rows of the text_clean column
+print(speech_df['text_clean'].head())
+# now your text strings have been standardized and cleaned up. 
+# You can now use this new column (text_clean) to extract information about the speeches
+---------
+
+# Find the length of each text
+speech_df['char_cnt'] = speech_df['text_clean'].str.len()
+
+# Count the number of words in each text
+speech_df['word_cnt'] = speech_df['text_clean'].str.split().str.len()
+
+# Find the average length of word
+speech_df['avg_word_length'] = speech_df['char_cnt'] / speech_df['word_cnt']
+
+# Print the first 5 rows of these columns
+print(speech_df[['text_clean', 'char_cnt', 'word_cnt', 'avg_word_length']])
+
+---------
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer 
+
+# Instantiate CountVectorizer
+cv = CountVectorizer()
+
+# Fit the vectorizer
+cv.fit(speech_df['text_clean'])
+
+# Print feature names
+print(cv.get_feature_names())
+
+# this vectorizer can be applied to both the text it was trained on, and new texts.
+------------
+
+# Apply the vectorizer
+cv_transformed = cv.transform(speech_df['text_clean'])
+
+# Print the full array
+cv_array = cv_transformed.toarray()
+print(cv_array)
+print(cv_array.shape)
+# he speeches have 9043 unique words, which is a lot! In the next exercise, you will see how to create a limited set of features
+-----
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Specify arguements to limit the number of features generated
+cv = CountVectorizer(min_df=0.2, max_df=0.8)
+
+# Fit, transform, and convert into array
+cv_transformed = cv.fit_transform(speech_df['text_clean'])
+cv_array = cv_transformed.toarray()
+
+# Print the array shape
+print(cv_array.shape)
+
+# Did you notice that the number of features (unique words) greatly reduced from 9043 to 818?
+------------
+# Create a DataFrame with these features
+cv_df = pd.DataFrame(cv_array, 
+                     columns=cv.get_feature_names()).add_prefix('Counts_')
+
+# Add the new columns to the original DataFrame
+speech_df_new = pd.concat([speech_df, cv_df], axis=1, sort=False)
+print(speech_df_new.head())
+# With the new features combined with the orginial DataFrame they can be now used for ML models or analysis
+
+------------
+# Import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Instantiate TfidfVectorizer
+tv = TfidfVectorizer(max_features=100, stop_words='english')
+
+# Fit the vectroizer and transform the data
+tv_transformed = tv.fit_transform(speech_df['text_clean'])
+
+# Create a DataFrame with these features
+tv_df = pd.DataFrame(tv_transformed.toarray(), 
+                     columns=tv.get_feature_names()).add_prefix('TFIDF_')
+print(tv_df.head())
+# Did you notice that counting the word occurences and calculating the Tf-idf weights are very similar? 
+# This is one of the reasons scikit-learn is very popular, a consistent API
+-------
+# Isolate the row to be examined
+sample_row = tv_df.iloc[0]
+
+# Print the top 5 words of the sorted output
+print(sample_row.sort_values(ascending=False).head())
+
+------
+# Instantiate TfidfVectorizer
+tv = TfidfVectorizer(max_features=100, stop_words='english')
+
+# Fit the vectroizer and transform the data
+tv_transformed = tv.fit_transform(train_speech_df['text_clean'])
+
+# Transform test data
+test_tv_transformed = tv.transform(test_speech_df['text_clean'])
+
+# Create new features for the test set
+test_tv_df = pd.DataFrame(test_tv_transformed.toarray(), 
+                          columns=tv.get_feature_names()).add_prefix('TFIDF_')
+print(test_tv_df.head())
+
+----------
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Instantiate a trigram vectorizer
+cv_trigram_vec = CountVectorizer(max_features=100, 
+                                 stop_words='english', 
+                                 ngram_range = (3,3))
+
+# Fit and apply trigram vectorizer
+cv_trigram = cv_trigram_vec.fit_transform(speech_df['text_clean'])
+
+# Print the trigram features
+print(cv_trigram_vec.get_feature_names())
+
+--------------
+# Create a DataFrame of the features
+cv_tri_df = pd.DataFrame(cv_trigram.toarray(), 
+                 columns=cv_trigram_vec.get_feature_names()).add_prefix('Counts_')
+
+# Print the top 5 words in the sorted output
+print(cv_tri_df.sum().sort_values(ascending=False).head())
+
+---------
