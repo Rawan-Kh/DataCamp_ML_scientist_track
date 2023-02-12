21608015a7a6107b4ee38f47f0765d9257d1ab72
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-12 11:27:16 +0200

    Update chapter2.py

--- extreme-gradient-boosting-with-xgboost/chapter2.py
+++ extreme-gradient-boosting-with-xgboost/chapter2.py
@@ -36,5 +36,109 @@ print("RMSE: %f" % (rmse))
 
 # it looks like linear base learners performed better!
 -------------
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-rmse-mean"]).tail(1))
+
+--------------
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics="mae", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-mae-mean"]).tail(1))
+
+------------
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+reg_params = [1, 10, 100]
+
+# Create the initial parameter dictionary for varying l2 strength: params
+params = {"objective":"reg:linear","max_depth":3}
+
+# Create an empty list for storing rmses as a function of l2 complexity
+rmses_l2 = []
+
+# Iterate over reg_params
+for reg in reg_params:
+
+    # Update l2 strength
+   params["lambda"] = reg
+
+    # Pass this updated param dictionary into cv
+   cv_results_rmse = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+
+    # Append best rmse (final round) to rmses_l2
+   rmses_l2.append(cv_results_rmse["test-rmse-mean"].tail(1).values[0])
+
+# Look at best rmse per l2 param
+print("Best rmse as a function of l2:")
+print(pd.DataFrame(list(zip(reg_params, rmses_l2)), columns=["l2","rmse"]))
+
+# It looks like as as the value of 'lambda' increases, so does the RMSE.
+
+-------------
+
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":2}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)
+
+# Plot the first tree
+xgb.plot_tree(xg_reg,num_trees=0)
+plt.show()
+
+# Plot the fifth tree
+xgb.plot_tree(xg_reg, num_trees=4)
+plt.show()
+
+# Plot the last tree sideways
+xgb.plot_tree(xg_reg, num_trees=9, rankdir="LR")
+plt.show()
+
+# Have a look at each of the plots. They provide insight into how the model arrived at 
+# its final decisions and what splits it made to arrive at those decisions. 
+# This allows us to identify which features are the most important in determining house price.
+# In the next exercise, you'll learn another way of visualizing feature importances.
+--------------
 
 
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)
+
+# Plot the feature importances
+xgb.plot_importance(xg_reg)
+plt.show()
+
+----------
