6d300d48c7a604baf8e99b6d44451d622c25899f
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-14 20:02:49 +0200

    Create chapter2.py

---
+++ model-validation-in-python/chapter2.py
@@ -0,0 +1,19 @@
+# Create dummy variables using pandas
+X = pd.get_dummies(tic_tac_toe.iloc[:,0:9])
+y = tic_tac_toe.iloc[:, 9]
+
+# Create training and testing datasets. Use 10% for the test set
+X_train, X_test, y_train,y_test  = train_test_split(X, y, test_size=.1, random_state=1111)
+
+-----------
+# Create temporary training and final testing datasets
+X_temp, X_test, y_temp, y_test  =\
+   train_test_split(X, y, test_size=0.20, random_state=1111)
+
+# Create the final training and validation datasets
+X_train, X_val, y_train, y_val  =\
+   train_test_split(X_temp, y_temp, test_size=0.25, random_state=1111)
+
+# You now have training, validation, and testing datasets, but do you know _when_ you need both validation and testing datasets?
+---------------
+
