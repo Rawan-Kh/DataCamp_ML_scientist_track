9b80b6ccb916018fff441677dd177639668a6097
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-17 09:47:26 +0200

    Create chapter2.py

---
+++ machine-learning-for-time-series-data-in-python/chapter2.py
@@ -0,0 +1,201 @@
+fig, axs = plt.subplots(3, 2, figsize=(15, 7), sharex=True, sharey=True)
+
+# Calculate the time array
+time = np.arange(normal.shape[0]) / sfreq
+
+# Stack the normal/abnormal audio so you can loop and plot
+stacked_audio = np.hstack([normal,abnormal]).T
+
+# Loop through each audio file / ax object and plot
+# .T.ravel() transposes the array, then unravels it into a 1-D vector for looping
+for iaudio, ax in zip(stacked_audio, axs.T.ravel()):
+    ax.plot(time, iaudio)
+show_plot_and_make_titles()
+
+#  As you can see there is a lot of variability in the raw data, let's see if you can average out some of that noise to notice a difference
+--------------
+
+# Average across the audio files of each DataFrame
+mean_normal = np.mean(normal, axis=1)
+mean_abnormal = np.mean(abnormal, axis=1)
+
+# Plot each average over time
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 3), sharey=True)
+ax1.plot(time, mean_normal)
+ax1.set(title="Normal Data")
+ax2.plot(time, mean_abnormal)
+ax2.set(title="Abnormal Data")
+plt.show()
+
+# Do you see a noticeable difference between the two? Maybe, but it's quite noisy. Let's see how you can dig into the data a bit further.
+----------
+
+from sklearn.svm import LinearSVC
+
+# Initialize and fit the model
+model = LinearSVC()
+model.fit(X_train, y_train)
+
+# Generate predictions and score them manually
+predictions = model.predict(X_test)
+print(sum(predictions == y_test.squeeze()) / len(y_test))
+
+# Note that your predictions didn't do so well. That's because the features you're using as inputs to the model (raw data) 
+# aren't very good at differentiating classes. Next, you'll explore how to calculate some more complex features that may improve the results
+-----------
+
+# Plot the raw data first
+audio.plot(figsize=(10, 5))
+plt.show()
+
+---------
+# Rectify the audio signal
+audio_rectified = audio.apply(np.abs)
+
+# Plot the result
+audio_rectified.plot(figsize=(10, 5))
+plt.show()
+
+-------
+# Smooth by applying a rolling mean
+audio_rectified_smooth = audio_rectified.rolling(50).mean()
+
+# Plot the result
+audio_rectified_smooth.plot(figsize=(10, 5))
+plt.show()
+# By calculating the envelope of each sound and smoothing it, you've eliminated much of the noise and have a
+# cleaner signal to tell you when a heartbeat is happening.
+-----------
+# Calculate stats
+means = np.mean(audio_rectified_smooth, axis=0)
+stds = np.std(audio_rectified_smooth, axis=0)
+maxs = np.max(audio_rectified_smooth, axis=0)
+
+# Create the X and y arrays
+X = np.column_stack([means, stds, maxs])
+y = labels.reshape(-1, 1)
+
+# Fit the model and score on testing data
+from sklearn.model_selection import cross_val_score
+percent_score = cross_val_score(model, X, y, cv=5)
+print(np.mean(percent_score))
+
+# This model is both simpler (only 3 features) and more understandable (features are simple summary statistics of the data).
+-------------
+
+# Calculate the tempo of the sounds
+tempos = []
+for col, i_audio in audio.items():
+    tempos.append(lr.beat.tempo(i_audio.values, sr=sfreq, hop_length=2**6, aggregate=None))
+
+# Convert the list to an array so you can manipulate it more easily
+tempos = np.array(tempos)
+
+# Calculate statistics of each tempo
+tempos_mean = tempos.mean(axis=-1)
+tempos_std = tempos.std(axis=-1)
+tempos_max = tempos.max(axis=-1)
+
+--------
+# Create the X and y arrays
+X = np.column_stack([means, stds, maxs, tempos_mean, tempos_std, tempos_max])
+y = labels.reshape(-1, 1)
+
+# Fit the model and score on testing data
+percent_score = cross_val_score(model, X, y, cv=5)
+print(np.mean(percent_score))
+# Note that your predictive power may not have gone up (because this dataset is quite small), 
+# but you now have a more rich feature representation of audio that your model can use!
+------------
+
+# Import the stft function
+from librosa.core import stft
+
+# Prepare the STFT
+HOP_LENGTH = 2**4
+spec = stft(audio, hop_length=HOP_LENGTH, n_fft=2**7)
+
+--------
+from librosa.core import amplitude_to_db
+from librosa.display import specshow
+
+# Convert into decibels
+spec_db = amplitude_to_db(spec)
+
+# Compare the raw audio to the spectrogram of the audio
+fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
+axs[0].plot(time, audio)
+specshow(spec_db, sr=sfreq, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH, ax=axs[1])
+plt.show()
+
+# Do you notice that the heartbeats come in pairs, as seen by the vertical lines in the spectrogram
+-----------
+
+import librosa as lr
+
+# Calculate the spectral centroid and bandwidth for the spectrogram
+bandwidths = lr.feature.spectral_bandwidth(S=spec)[0]
+centroids = lr.feature.spectral_centroid(S=spec)[0]
+
+# -----------sol.py
+from librosa.core import amplitude_to_db
+from librosa.display import specshow
+
+# Convert spectrogram to decibels for visualization
+spec_db = amplitude_to_db(spec)
+
+# Display these features on top of the spectrogram
+fig, ax = plt.subplots(figsize=(10, 5))
+specshow(spec_db, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH, ax=ax)
+ax.plot(times_spec, centroids)
+ax.fill_between(times_spec, centroids - bandwidths / 2, centroids + bandwidths / 2, alpha=.5)
+ax.set(ylim=[None, 6000])
+plt.show()
+# -----------------------mine
+from librosa.core import amplitude_to_db
+from librosa.display import specshow
+
+# Convert spectrogram to decibels for visualization
+spec_db = amplitude_to_db(spec)
+
+# Display these features on top of the spectrogram
+fig, ax = plt.subplots(figsize=(10, 5))
+ax = specshow(spec_db, x_axis='time', y_axis='hz', hop_length=HOP_LENGTH)
+ax.plot(times_spec, centroids)
+ax.fill_between(times_spec, centroids - bandwidths / 2, centroids + bandwidths / 2, alpha=.5)
+ax.set(ylim=[None, 6000])
+plt.show()
+
+# As you can see, the spectral centroid and bandwidth characterize the spectral content in each sound over time.
+# They give us a summary of the spectral content that we can use in a classifier.
+-----------------------
+
+# Loop through each spectrogram
+bandwidths = []
+centroids = []
+
+for spec in spectrograms:
+    # Calculate the mean spectral bandwidth
+    this_mean_bandwidth = np.mean(lr.feature.spectral_bandwidth(S=spec))
+    # Calculate the mean spectral centroid
+    this_mean_centroid = np.mean(lr.feature.spectral_centroid(S=spec))
+    # Collect the values
+    bandwidths.append(this_mean_bandwidth)  
+    centroids.append(this_mean_centroid)
+    
+--------------
+# Create X and y arrays
+X = np.column_stack([means, stds, maxs, tempo_mean, tempo_max, tempo_std, bandwidths, centroids])
+y = labels.reshape(-1, 1)
+
+# Fit the model and score on testing data
+percent_score = cross_val_score(model, X, y, cv=5)
+print(np.mean(percent_score))
+
+# You calculated many different features of the audio, and combined each of them under the assumption that
+# they provide independent information that can be used in classification.
+# You may have noticed that the accuracy of your models varied a lot when using different set of features. 
+# This chapter was focused on creating new "features" from raw data and not obtaining the best accuracy. 
+# To improve the accuracy, you want to find the right features that provide relevant information and also build models on much larger data
+
+--------------
