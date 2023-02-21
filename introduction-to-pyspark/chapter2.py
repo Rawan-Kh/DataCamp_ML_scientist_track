25c50a116e2fc2201156a52f85f54c5b3feb2659
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-21 16:30:23 +0200

    Update chapter2.py

--- introduction-to-pyspark/chapter2.py
+++ introduction-to-pyspark/chapter2.py
@@ -20,5 +20,82 @@ long_flights1.show()
 long_flights2.show()
 # PySpark often provides a few different ways to get the same results.
 -------------
+# Select the first set of columns
+selected1 = flights.select("tailnum", "origin", "dest")
 
+# Select the second set of columns
+temp = flights.select(flights.origin, flights.dest, flights.carrier)
+
+# Define first filter
+filterA = flights.origin == "SEA"
+
+# Define second filter
+filterB = flights.dest == "PDX"
+
+# Filter the data, first by filterA then by filterB
+selected2 = temp.filter(filterA).filter(filterB)
+
+-------
+# Define avg_speed
+avg_speed = (flights.distance/(flights.air_time/60)).alias("avg_speed")
+
+# Select the correct columns
+speed1 = flights.select("origin", "dest", "tailnum", avg_speed)
+
+# Create the same table using a SQL expression
+speed2 = flights.selectExpr("origin", "dest", "tailnum", "distance/(air_time/60) as avg_speed")
+
+--------
+# Find the shortest flight from PDX in terms of distance
+flights.filter(flights.origin=='PDX').groupBy().min('distance').show()
+
+# Find the longest flight from SEA in terms of air time
+flights.filter(flights.origin=='SEA').groupBy().max('air_time').show()
+---------
+# Average duration of Delta flights
+flights.filter(flights.carrier=="DL").filter(flights.origin == "SEA").groupBy().avg("air_time").show()
+
+# Total hours in the air
+flights.withColumn("duration_hrs", flights.air_time/60).groupBy().sum("duration_hrs").show()
+# Now you can answer some interesting questions about the data.
+--------
+
+# Group by tailnum
+by_plane = flights.groupBy("tailnum")
+
+# Number of flights each plane made
+by_plane.count().show()
+
+# Group by origin
+by_origin = flights.groupBy("origin")
+
+# Average duration of flights from PDX and SEA
+by_origin.avg("air_time").show()
+--------
+# Import pyspark.sql.functions as F
+import pyspark.sql.functions as F
+
+# Group by month and dest
+by_month_dest = flights.groupBy('month','dest')
+
+# Average departure delay by month and destination
+by_month_dest.avg('dep_delay').show()
+
+# Standard deviation of departure delay
+by_month_dest.agg(F.stddev('dep_delay')).show()
+-----------
+
+# Examine the data
+print(airports.show())
+
+# Rename the faa column
+airports = airports.withColumnRenamed("faa", "dest")
+
+# Join the DataFrames
+flights_with_airports = flights.join(airports,on='dest',how="leftouter")
+
+# Examine the new DataFrame
+print(flights_with_airports.show())
+
+----------
 
