ab7a460c3c35e3a62f6cd0ada7864013d16855f0
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-21 14:45:00 +0200

    Create chapter2.py

---
+++ introduction-to-pyspark/chapter2.py
@@ -0,0 +1,24 @@
+# Create the DataFrame flights
+flights = spark.table("flights")
+
+# Show the head
+flights.show()
+
+# Add duration_hrs
+flights = flights.withColumn("duration_hrs", flights.air_time/60)
+# Now you can make new columns derived from the old ones!
+------------
+
+# Filter flights by passing a string
+long_flights1 = flights.filter("distance > 1000")
+
+# Filter flights by passing a column of boolean values
+long_flights2 = flights.filter(flights.distance > 1000)
+
+# Print the data to check they're equal
+long_flights1.show()
+long_flights2.show()
+# PySpark often provides a few different ways to get the same results.
+-------------
+
+
