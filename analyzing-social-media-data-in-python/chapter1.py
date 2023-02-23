071a2be25779d0128cfd5c3cb72585d1d8748c63
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2023-02-23 18:39:27 +0200

    Create chapter1.py

---
+++ analyzing-social-media-data-in-python/chapter1.py
@@ -0,0 +1,7 @@
+# You've been asked to identify the success (or failure) of a particular product. What Twitter analysis strategy could you use to best execute this?
+
+# Collect mentions of the product and identify if people are talking about it positively.
+# Examine the size of the retweet network mentioning the product.
+# Analyzing the geographical penetration of users mentioning the product.
+-------------
+
