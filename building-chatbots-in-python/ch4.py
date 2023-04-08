4afc7c92425e96a47aecae483ac1b6affa1c796a
Rawan Khalid <rookhsalama@gmail.com>
2023-04-08 10:46:18 +0200

    Update ch4.py

--- building-chatbots-in-python/ch4.py
+++ building-chatbots-in-python/ch4.py
@@ -111,3 +111,28 @@ def policy(intent):
 
 #  With a policy() function defined, you can now incorporate it into a send_message() function.      
 ----------------
+
+# Define send_message()
+def send_message(pending , message):
+    print("USER : {}".format(message))
+    action, pending_action = policy(interpret(message))
+    if action == "do_pending" and pending is not None:
+        print("BOT : {}".format(pending))
+    else:
+        print("BOT : {}".format(action))
+    return pending_action
+    
+# Define send_messages()
+def send_messages(messages):
+    pending = None
+    for msg in messages:
+        pending = send_message(pending, msg)
+
+# Send the messages
+send_messages([
+    "I'd like to order some coffee",
+    "ok yes please"
+])
+# Your bot can now follow up on its own suggestions!
+-------------
+
