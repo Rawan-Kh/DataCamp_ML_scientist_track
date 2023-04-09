c9c632573ddec62c580f8ac3030aed30412a281f
Rawan Khalid <rookhsalama@gmail.com>
2023-04-09 14:43:39 +0200

    Update ch2.py

--- advanced-nlp-with-spacy/ch2.py
+++ advanced-nlp-with-spacy/ch2.py
@@ -115,5 +115,131 @@ for token in doc:
             print('Found a verb after a proper noun!')
 -------------
 
+# Load the en_core_web_md model
+nlp = spacy.load('en_core_web_md')
 
+# Process a text
+doc = nlp("Two bananas in pyjamas")
 
+# Get the vector for the token "bananas"
+bananas_vector = doc[1].vector
+print(bananas_vector)
+
+# In the next exercise, you'll be using spaCy to predict similarities between documents, spans and tokens via the word vectors under the hood.
+-------------
+
+doc1 = nlp("It's a warm summer day")
+doc2 = nlp("It's sunny outside")
+
+# Get the similarity of doc1 and doc2
+similarity = doc1.similarity(doc2)
+print(similarity)
+
+---------
+doc = nlp("TV and books")
+token1, token2 = doc[0], doc[2]
+
+# Get the similarity of the tokens "TV" and "books" 
+similarity = token1.similarity(token2)
+print(similarity)
+-----------
+doc = nlp("This was a great restaurant. Afterwards, we went to a really nice bar.")
+
+# Create spans for "great restaurant" and "really nice bar"
+span1 = doc[3:5]
+span2 = doc[12:15]
+
+# Get the similarity of the spans
+similarity = span1.similarity(span2)
+print(similarity)
+# Feel free to experiment with comparing more objects, if you like. The similarities are not always this conclusive. Once you're getting serious about developing NLP applications that leverage semantic similarity, you might want to train vectors on your own data, or tweak the similarity algorithm.
+-------------
+
+# Why does this pattern not match the tokens "Silicon Valley" in the doc?
+
+# pattern = [{'LOWER': 'silicon'}, {'TEXT': ' '}, {'LOWER': 'valley'}]
+# doc = nlp("Can Silicon Valley workers rein in big tech from within?")
+------------
+
+# The tokenizer doesn't create tokens for single spaces, so there's no token with the value ' ' in between.
+# Correct! The tokenizer already takes care of splitting off whitespace and each dictionary in the pattern describes one token.
+-----------
+# Create the match patterns
+pattern1 = [{'LOWER': 'amazon'}, {'IS_TITLE': True, 'POS': 'PROPN'}]
+pattern2 = [{'LOWER': 'ad'}, {'TEXT': '-'}, {'LOWER': 'free'}, {'POS': 'NOUN'}]
+
+# Initialize the Matcher and add the patterns
+matcher = Matcher(nlp.vocab)
+matcher.add('PATTERN1', None, pattern1)
+matcher.add('PATTERN2', None, pattern2)
+
+# Iterate over the matches
+for match_id, start, end in matcher(doc):
+    # Print pattern string name and text of matched span
+    print(doc.vocab.strings[match_id], doc[start:end].text)
+    
+# Well done! For the token '_', you can match on the attribute TEXT, LOWER or even SHAPE. All of those are correct. As you can see, paying close attention to the tokenization is very important when working with the token-based Matcher. Sometimes it's much easier to just match exact strings instead and use the PhraseMatcher, which we'll get to in the next exercise.
+-------------
+
+# Import the PhraseMatcher and initialize it
+from spacy.matcher import PhraseMatcher
+matcher = PhraseMatcher(nlp.vocab)
+
+# Create pattern Doc objects and add them to the matcher
+# This is the faster version of: [nlp(country) for country in COUNTRIES]
+patterns = list(nlp.pipe(COUNTRIES))
+matcher.add('COUNTRY', None, *patterns)
+
+# Call the matcher on the test document and print the result
+matches = matcher(doc)
+print([doc[start:end] for match_id, start, end in matches])
+---------------------
+# Create a doc and find matches in it
+doc = nlp(text)
+
+# Iterate over the matches
+for match_id, start, end in matcher(doc):
+    # Create a Span with the label for "GPE"
+    span = Span(doc, start, end, label='GPE')
+
+    # Overwrite the doc.ents and add the span
+    doc.ents = list(doc.ents) + [span]
+
+# Print the entities in the document
+print([(ent.text, ent.label_) for ent in doc.ents if ent.label_ == 'GPE'])
+
+# output:
+#     [('Namibia', 'GPE'), ('South Africa', 'GPE'), ('Cambodia', 'GPE'), ('Kuwait', 'GPE'), ('Somalia', 'GPE'), ('Haiti', 'GPE'), ('Mozambique', 'GPE'), ('Somalia', 'GPE'), ('Rwanda', 'GPE'), ('Singapore', 'GPE'), ('Sierra Leone', 'GPE'), ('Afghanistan', 'GPE'), ('Iraq', 'GPE'), ('Sudan', 'GPE'), ('Congo', 'GPE'), ('Haiti', 'GPE')]
+------------
+# Create a doc and find matches in it
+doc = nlp(text)
+
+# Iterate over the matches
+for match_id, start, end in matcher(doc):
+    # Create a Span with the label for "GPE" and overwrite the doc.ents
+    span = Span(doc, start, end, label='GPE')
+    doc.ents = list(doc.ents) + [span]
+    
+    # Get the span's root head token
+    span_root_head = span.root.head
+    # Print the text of the span root's head token and the span text
+    print(span_root_head.text, '-->', span.text)
+ 
+# output:
+#     Namibia --> Namibia
+#     South --> South Africa
+#     Cambodia --> Cambodia
+#     Kuwait --> Kuwait
+#     Somalia --> Somalia
+#     Haiti --> Haiti
+#     Mozambique --> Mozambique
+#     Somalia --> Somalia
+#     Rwanda --> Rwanda
+#     Singapore --> Singapore
+#     Sierra --> Sierra Leone
+#     Afghanistan --> Afghanistan
+#     Iraq --> Iraq
+#     Sudan --> Sudan
+#     Congo --> Congo
+#     Haiti --> Haiti
+------------
