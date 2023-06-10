4fc037fdb19ca3f636d6ca3d3e9e2f720d0cb73a
Rawan Khalid <rookhsalama@gmail.com>
2023-06-10 22:33:20 +0300

    Create ch1.py

---
+++ Deep_learning/advanced-deep-learning-with-keras/ch1.py
@@ -0,0 +1,7 @@
+# Import Input from tensorflow.keras.layers
+from tensorflow.keras.layers import Input
+
+# Create an input layer of shape 1
+input_tensor = Input(shape=(1,))
+# Remember that the input layer allows your model to load data.
+-------
