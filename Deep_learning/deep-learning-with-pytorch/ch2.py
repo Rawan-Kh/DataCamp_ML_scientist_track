34cc735bf68ebb50619be44736cc95f4bd3c1ec0
Rawan Khalid <rookhsalama@gmail.com>
2023-06-12 20:10:20 +0300

    Update ch2.py

--- Deep_learning/deep-learning-with-pytorch/ch2.py
+++ Deep_learning/deep-learning-with-pytorch/ch2.py
@@ -57,4 +57,119 @@ print(torch.matmul(hidden_1_activated, weight_2))
 
 #  you computed correctly the softmax cross-entropy loss. You rock!
 --------
+# Initialize the scores and ground truth
+logits = torch.tensor([[-1.2, 0.12, 4.8]])
+ground_truth = torch.tensor([2])
+
+# Instantiate cross entropy loss
+criterion = nn.CrossEntropyLoss()
+
+# Compute and print the loss
+loss = criterion(logits,ground_truth)
+print(loss)
+-----------
+
+# Import torch and torch.nn
+import torch 
+import torch.nn as nn
+
+# Initialize logits and ground truth
+logits = torch.rand(1, 1000)
+ground_truth = torch.tensor([111])
+
+# Instantiate cross-entropy loss
+criterion = nn.CrossEntropyLoss()
+
+# Calculate and print the loss
+loss = criterion(logits,ground_truth)
+print(loss)
+
+---------
+# Transform the data to torch tensors and normalize it 
+transform = transforms.Compose([transforms.ToTensor(),
+								transforms.Normalize((0.1307), ((0.3081)))])
+
+# Prepare training set and testing set
+trainset = torchvision.datasets.MNIST('mnist', train=True, 
+									  download=True, transform=transform)
+testset = torchvision.datasets.MNIST('mnist', train=False,
+			   download=True, transform=transform)
+
+# Prepare training loader and testing loader
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32,
+                                          shuffle=True, num_workers=0)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32,
+										  shuffle=False, num_workers=0) 
+
+-----------
+# Compute the shape of the training set and testing set
+trainset_shape = trainloader.dataset.train_data.shape
+testset_shape = testloader.dataset.test_data.shape
+
+# Print the computed shapes
+print(trainset_shape, testset_shape)
+
+# Compute the size of the minibatch for training set and testing set
+trainset_batchsize = trainloader.batch_size
+testset_batchsize = testloader.batch_size
+
+# Print sizes of the minibatch
+print(trainset_batchsize, testset_batchsize)
+# output:
+#     torch.Size([60000, 28, 28]) torch.Size([10000, 28, 28])
+#     32 32
+--------------
+# Define the class Net
+class Net(nn.Module):
+    def __init__(self):    
+    	# Define all the parameters of the net
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28 * 1, 200)
+        self.fc2 = nn.Linear(200, 10)
+
+    def forward(self, x):   
+    	# Do the forward pass
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+   
+ ------------
+
+# Instantiate the Adam optimizer and Cross-Entropy loss function
+model = Net()   
+optimizer = optim.Adam(model.parameters(), lr=3e-4)
+criterion = nn.CrossEntropyLoss()
+  
+for batch_idx, data_target in enumerate(train_loader):
+    data = data_target[0]
+    target = data_target[1]
+    data = data.view(-1, 28 * 28)
+    optimizer.zero_grad()
+
+    # Complete a forward pass
+    output = model(data)
+
+    # Compute the loss, gradients and change the weights
+    loss =  criterion(output, target)
+    loss.backward()
+    optimizer.step()
+    
+---------------
+# Set the model in eval mode
+model.eval()
+
+for i, data in enumerate(test_loader, 0):
+    inputs, labels = data
+    
+    # Put each image into a vector
+    inputs = inputs.view(-1, 784)
+    
+    # Do the forward pass and get the predictions
+    outputs = model(inputs)
+    _, outputs = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (outputs == labels).sum().item()
+print('The testing set accuracy of the network is: %d %%' % (100 * correct / total))
+
+----------
 
